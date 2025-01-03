8ba51633be92e1935810dfac934548feda8f14d1
Evren Yeniev <mail@evren.dev>
2025-01-03 09:57:33 +0100

    Add domain layer with base entity, auditable entity, and value object implementations

---
+++ src/backend/Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace EvrenDev.Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTimeOffset CreatedTime { get; set; }
+
+    public string? Creator { get; set; }
+
+    public DateTimeOffset ModifiedTime { get; set; }
+
+    public string? Modifier { get; set; }
+
+    public bool Deleted { get; set; }
+
+    public string? Deleter { get; set; }
+
+    public DateTimeOffset? DeletionTime { get; set; }
+}
---
+++ src/backend/Domain/Common/BaseEvent.cs
@@ -0,0 +1,5 @@
+﻿namespace EvrenDev.Domain.Common;
+
+public abstract class BaseEvent : INotification
+{
+}
