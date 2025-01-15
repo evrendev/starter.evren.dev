540dcaf1fa63ac2b50b680fb74aa248cac063548
Evren Yeniev <mail@evren.dev>
2025-01-15 10:16:19 +0100

    Refactor TodoItemDto to BasicTodoItemDto; update related models and queries for consistency

--- src/backend/Application/Features/TodoLists/Models/TodoListDto.cs
+++ src/backend/Application/Features/TodoLists/Models/TodoListDto.cs
@@ -5,5 +5,5 @@ public class TodoListDto
     public Guid Id { get; set; }
     public string? Title { get; set; }
     public Colour Colour { get; set; } = Colour.White;
-    public List<TodoItemDto> Items { get; set; } = new();
+    public List<BasicTodoItemDto> Items { get; set; } = new();
 }
