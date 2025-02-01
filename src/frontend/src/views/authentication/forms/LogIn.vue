678fa8a77e58dd5d47681500522d74bcbea9918c
Evren Yeniev <mail@evren.dev>
2025-02-01 12:32:02 +0100

    Refactor LogIn component to streamline reset button implementation and improve code readability

--- src/frontend/src/views/authentication/forms/LogIn.vue
+++ src/frontend/src/views/authentication/forms/LogIn.vue
@@ -117,22 +117,14 @@ const onSubmit = handleSubmit(async (values) => {
     </div>
 
     <div class="d-flex justify-center justify-md-start mt-7 mb-lg-2 mb-sm-0">
+      <v-btn color="secondary" variant="flat" class="ml-4" @click="resetForm()" prepend-icon="$refresh" :disabled="loading">
+        {{ t("auth.login.resetForm") }}
+      </v-btn>
       <challenge-v3 v-model="response" action="submit">
-        <v-btn color="primary" variant="flat" type="submit" :loading="loading" density="comfortable" prepend-icon="$login">
+        <v-btn color="primary" variant="flat" type="submit" :loading="loading" prepend-icon="$login">
           {{ t("auth.login.submit") }}
         </v-btn>
       </challenge-v3>
-      <v-btn
-        color="secondary"
-        variant="flat"
-        class="ml-4"
-        @click="resetForm()"
-        density="comfortable"
-        prepend-icon="$refresh"
-        :disabled="loading"
-      >
-        {{ t("auth.login.resetForm") }}
-      </v-btn>
     </div>
   </v-form>
 </template>
