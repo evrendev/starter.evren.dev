6f2d039b6b3a1b50862258bfafa0abe57a61a5ab
Evren Yeniev <mail@evren.dev>
2025-02-05 15:17:43 +0100

    Add predefined values for gender and languages; implement PredefinedValuesController and related query handling

---
+++ src/backend/Application/Common/Models/PredefinedValueDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EvrenDev.Application.Common.Models;
+
+public class PredefinedValueDto<T>
+{
+    public T? Code { get; init; }
+
+    public string? Name { get; init; }
+}
---
+++ src/backend/Application/Features/PredefinedValues/Queries/PredefinedValuesVm.cs
@@ -0,0 +1,7 @@
+﻿namespace EvrenDev.Application.Features.PredefinedValues.Queries;
+
+public class PredefinedValuesVm
+{
+    public IReadOnlyCollection<PredefinedValueDto<string>> Genders { get; init; } = Array.Empty<PredefinedValueDto<string>>();
+    public IReadOnlyCollection<PredefinedValueDto<string>> Languages { get; init; } = Array.Empty<PredefinedValueDto<string>>();
+}
