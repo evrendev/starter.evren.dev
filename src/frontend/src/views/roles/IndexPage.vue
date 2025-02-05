608eac77e9fc0777180f5d1b42c8c01707c60f04
Evren Yeniev <mail@evren.dev>
2025-02-05 11:22:17 +0100

    Enhance role management: add reset watcher to reload table and implement search options for improved data handling

--- src/frontend/src/views/roles/IndexPage.vue
+++ src/frontend/src/views/roles/IndexPage.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, shallowRef } from "vue";
+import { ref, watch, shallowRef } from "vue";
 import { useRoleStore } from "@/stores";
 import { useI18n } from "vue-i18n";
+import { storeToRefs } from "pinia";
 import { DataTable } from "./components";
 import { Breadcrumb } from "@/components/forms";
 
@@ -19,9 +20,32 @@ const items = ref([]);
 const itemsLength = ref(0);
 const loading = ref(false);
 const roleStore = useRoleStore();
+const { reset } = storeToRefs(roleStore);
+
+watch(
+  () => reset.value,
+  () => {
+    if (reset.value) {
+      loading.value = true;
+      reloadTable(1000);
+    }
+  }
+);
+
+const searchOptions = ref({});
+
+const reloadTable = (timeout) => {
+  loading.value = true;
+
+  setTimeout(() => {
+    getItems(searchOptions.value);
+  }, timeout);
+};
 
 const getItems = async (options) => {
   loading.value = true;
+  searchOptions.value = options;
+
   await roleStore.getItems(options);
   items.value = roleStore.items;
   itemsLength.value = roleStore.itemsLength;
