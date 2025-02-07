66fe7632454fda99651b245238d7922e598342c5
Evren Yeniev <mail@evren.dev>
2025-02-07 14:16:15 +0100

    Update Initial property in UserDto to handle empty names and restore captcha validation in LoginCommand

--- src/backend/Application/Common/Models/UserDto.cs
+++ src/backend/Application/Common/Models/UserDto.cs
@@ -7,7 +7,7 @@ public class UserDto
     public Guid Id { get; set; }
     public Guid? TenantId { get; set; }
     public string? Gender { get; set; } = Defaults.Gender.Code;
-    public string Initial => $"{FirstName}{LastName}";
+    public string Initial => !string.IsNullOrEmpty(FirstName) || !string.IsNullOrEmpty(LastName) ? $"{FirstName?[0]}{LastName?[0]}" : string.Empty;
     public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
