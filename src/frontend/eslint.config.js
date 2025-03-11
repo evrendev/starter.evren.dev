507c314fc5a6e9106b7a668cbc84e10214d8239d
Evren Yeniev <mail@evren.dev>
2025-03-11 10:22:45 +0100

    Remove Vitest configuration and update dependencies in package.json

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
-import pluginVitest from "@vitest/eslint-plugin";
 import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
 
 export default [
@@ -17,10 +16,6 @@ export default [
   js.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 
-  {
-    ...pluginVitest.configs.recommended,
-    files: ["src/**/__tests__/*"]
-  },
   skipFormatting,
 
   // Add browser environment to recognize browser globals
