9645c5a5d440d6a4e8483f3844767d98fc00fdce
Evren Yeniev <mail@evren.dev>
2025-04-07 16:19:41 +0200

    feat(auth): restructure AuthRoutes to include children for better routing organization

--- src/frontend/src/router/auth-routes.js
+++ src/frontend/src/router/auth-routes.js
@@ -1,13 +1,19 @@
 const AuthRoutes = {
-  name: "login",
-  path: "/auth/:page?",
-  component: () => import("@/views/authentication/IndexPage.vue"),
-  props: (route) => ({
-    page: route.params.page || "login"
-  }),
-  meta: {
-    titleKey: "auth.login.title"
-  }
+  path: "/auth",
+  component: () => import("@/layouts/blank/BlankLayout.vue"),
+  children: [
+    {
+      path: ":page?",
+      name: "login",
+      component: () => import("@/views/authentication/IndexPage.vue"),
+      props: (route) => ({
+        page: route.params.page || "login"
+      }),
+      meta: {
+        titleKey: "auth.login.title"
+      }
+    }
+  ]
 };
 
 export default AuthRoutes;
