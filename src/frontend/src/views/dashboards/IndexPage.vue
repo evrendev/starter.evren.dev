a89d50e90eb202c439dcd0a77cac8713c800dbac
Evren Yeniev <mail@evren.dev>
2025-04-25 14:39:18 +0200

    feat(dashboard): add DoughnutCard component and integrate chart data visualization

--- src/frontend/src/views/dashboards/IndexPage.vue
+++ src/frontend/src/views/dashboards/IndexPage.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { useI18n } from "vue-i18n";
-import { onMounted, ref, shallowRef } from "vue";
+import { computed, onMounted, ref, shallowRef } from "vue";
 import { Breadcrumb } from "@/components/forms";
 import { useFountainDonationStore } from "@/stores";
 import { storeToRefs } from "pinia";
 import CountsCard from "./components/ProjectCountCard.vue";
+import DoughnutCard from "./components/DoughnutCard.vue";
 
 const { t } = useI18n();
 
@@ -13,6 +14,32 @@ const { counts: projects } = storeToRefs(fountainDonationStore);
 
 onMounted(() => {
   fountainDonationStore.getCounts();
+});
+
+const options = ref({
+  responsive: true,
+  maintainAspectRatio: false
+});
+
+const colorMap = {
+  primary: "#1976D2",
+  warning: "#FB8C00",
+  info: "#2196F3",
+  error: "#E53935"
+};
+
+const chartData = computed(() => {
+  if (!projects.value?.length) return null;
+
+  return {
+    labels: projects.value.map((p) => p.project.name),
+    datasets: [
+      {
+        data: projects.value.map((p) => p.count),
+        backgroundColor: projects.value.map((p) => colorMap[p.project.color] || "#CCCCCC")
+      }
+    ]
+  };
 });
 
 const page = ref({ title: t("components.sidebar.dashboard.title") });
@@ -28,8 +55,11 @@ const breadcrumbs = shallowRef([
 <template>
   <breadcrumb :title="page.title" :breadcrumbs="breadcrumbs" />
   <v-row>
-    <v-col cols="3" xs="12" v-for="(item, index) in projects" :key="index">
-      <counts-card :item="item" />
+    <v-col cols="8" xs="12">
+      <counts-card v-for="(item, index) in projects" :key="index" :item="item" class="mb-2" />
+    </v-col>
+    <v-col cols="4" xs="12">
+      <doughnut-card :chart-data="chartData" :options="options" />
     </v-col>
   </v-row>
 </template>
---
+++ src/frontend/src/views/dashboards/components/DoughnutCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "vue-chartjs";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+defineProps({
+  chartData: {
+    type: Object,
+    default: () => ({}),
+    required: false
+  },
+  options: {
+    type: Object,
+    default: () => ({}),
+    required: false
+  }
+});
+</script>
+
+<template>
+  <Doughnut v-if="chartData" :data="chartData" :options="options" />
+</template>
