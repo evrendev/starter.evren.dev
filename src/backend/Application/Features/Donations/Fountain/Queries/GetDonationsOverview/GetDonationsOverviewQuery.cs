760e709153ced05bdac2522dd3c25a762d485451
Evren Yeniev <mail@evren.dev>
2025-05-21 16:32:31 +0200

    fix(donations): refactor GetDonationsOverviewQueryHandler to improve query structure and exception handling

--- src/backend/Application/Features/Donations/Fountain/Queries/GetDonationsOverview/GetDonationsOverviewQuery.cs
+++ src/backend/Application/Features/Donations/Fountain/Queries/GetDonationsOverview/GetDonationsOverviewQuery.cs
@@ -47,91 +47,81 @@ public class GetDonationsOverviewQueryHandler : IRequestHandler<GetDonationsOver
 
     public async Task<Result<DonationOverview?>> Handle(GetDonationsOverviewQuery request, CancellationToken cancellationToken)
     {
-        try
-        {
-            var query = _context.FountainDonations.AsQueryable().Where(donation => donation.Source != "EMPTY");
+        var query = _context.FountainDonations.AsQueryable().Where(donation => donation.Source != "EMPTY");
 
-            if (request.StartDate != null)
-                query = query.Where(entity => entity.CreationDate >= request.StartDate);
+        if (request.StartDate != null)
+            query = query.Where(entity => entity.CreationDate >= request.StartDate);
 
-            if (request.EndDate != null)
-                query = query.Where(entity => entity.CreationDate <= request.EndDate);
+        if (request.EndDate != null)
+            query = query.Where(entity => entity.CreationDate <= request.EndDate);
 
-            if (!string.IsNullOrEmpty(request.Project))
-                query = query.Where(entity => entity.Project == request.Project);
+        if (!string.IsNullOrEmpty(request.Project))
+            query = query.Where(entity => entity.Project == request.Project);
 
-            var stats = await query
-                .GroupBy(x => x.Project)
-                .OrderBy(x => x.Key)
-                .Select(g => new ProjectsCountDto
-                {
-                    Project = FountainDonationProject.FromName(g.Key),
-                    Count = g.Count()
-                })
-                .ToListAsync(cancellationToken);
-
-            var groupedDonations = await query
-                .GroupBy(d => d.Project)
-                .ToListAsync(cancellationToken);
-
-            var donations = groupedDonations
-                .SelectMany(g => g
-                    .OrderByDescending(d => d.CreationDate)
-                    .Take(3)
-                )
-                .OrderBy(d => d.Project)
-                .Select(entity => new BasicFountainDonationDto
-                {
-                    Id = entity.Id,
-                    Contact = entity.Contact,
-                    Phone = Tools.CreatePhone(entity.Phone, $"{entity.Project}-{entity.ProjectNumber}", entity.Banner),
-                    CreationDate = DateTimeDto.Create.FromUtc(entity.CreationDate),
-                    HtmlBanner = $"<strong>{entity.Project}-{entity.ProjectNumber}:</strong> {entity.Banner}",
-                    PlainBanner = $"{entity.Project}-{entity.ProjectNumber}: {entity.Banner}",
-                    Team = FountaionTeam.From(entity.Team),
-                    MediaStatus = MediaStatus.From(entity.MediaStatus),
-                    MediaInformation = entity.MediaInformation,
-                })
-                .ToList();
-
-            var monthlyProjectStats = await query
-                .GroupBy(d => new { d.CreationDate.Year, d.CreationDate.Month, d.Project })
-                .Select(g => new
-                {
-                    Year = g.Key.Year,
-                    MonthNumber = g.Key.Month,
-                    Project = g.Key.Project,
-                    Count = g.Count()
-                })
-                .ToListAsync(cancellationToken);
-
-            var formattedStats = monthlyProjectStats
-                .Select(g => new MonthlyProjectStatsDto
-                {
-                    Month = new DateTime(g.Year, g.MonthNumber, 1).ToString("MMM"),
-                    Project = g.Project,
-                    Count = g.Count
-                })
-                .OrderBy(g => g.Month)
-                .ThenBy(g => g.Project)
-                .ToList();
-
-
-            var response = new DonationOverview()
+        var stats = await query
+            .GroupBy(x => x.Project)
+            .OrderBy(x => x.Key)
+            .Select(g => new ProjectsCountDto
             {
-                Stats = stats,
-                Donations = donations,
-                MonthlyProjectStats = formattedStats
-            };
+                Project = FountainDonationProject.FromName(g.Key),
+                Count = g.Count()
+            })
+            .ToListAsync(cancellationToken);
 
-            return Result<DonationOverview?>.Success(response);
-        }
-        catch (Exception? ex)
+        var groupedDonations = await query
+            .GroupBy(d => d.Project)
+            .ToListAsync(cancellationToken);
+
+        var donations = groupedDonations
+            .SelectMany(g => g
+                .OrderByDescending(d => d.CreationDate)
+                .Take(3)
+            )
+            .OrderBy(d => d.Project)
+            .Select(entity => new BasicFountainDonationDto
+            {
+                Id = entity.Id,
+                Contact = entity.Contact,
+                Phone = Tools.CreatePhone(entity.Phone, $"{entity.Project}-{entity.ProjectNumber}", entity.Banner),
+                CreationDate = DateTimeDto.Create.FromUtc(entity.CreationDate),
+                HtmlBanner = $"<strong>{entity.Project}-{entity.ProjectNumber}:</strong> {entity.Banner}",
+                PlainBanner = $"{entity.Project}-{entity.ProjectNumber}: {entity.Banner}",
+                Team = FountaionTeam.From(entity.Team),
+                MediaStatus = MediaStatus.From(entity.MediaStatus),
+                MediaInformation = entity.MediaInformation,
+            })
+            .ToList();
+
+        var monthlyProjectStats = await query
+            .GroupBy(d => new { d.CreationDate.Year, d.CreationDate.Month, d.Project })
+            .Select(g => new
+            {
+                g.Key.Year,
+                g.Key.Month,
+                g.Key.Project,
+                Count = g.Count()
+            })
+            .ToListAsync(cancellationToken);
+
+        var formattedStats = monthlyProjectStats
+            .Select(g => new MonthlyProjectStatsDto
+            {
+                Month = new DateTime(g.Year, g.Month, 1).ToString("MMM", CultureInfo.InvariantCulture),
+                Project = g.Project,
+                Count = g.Count
+            })
+            .OrderBy(g => g.Month)
+            .ThenBy(g => g.Project)
+            .ToList();
+
+
+        var response = new DonationOverview()
         {
-            // Log the exception (if logging is set up)
-            _logger.LogError(ex, "An error occurred while handling GetDonationsOverviewQuery.");
-            // Return a failure result
-            return Result<DonationOverview?>.Failure("An error occurred while processing your request.");
-        }
+            Stats = stats,
+            Donations = donations,
+            MonthlyProjectStats = formattedStats
+        };
+
+        return Result<DonationOverview?>.Success(response);
     }
 }
