6cbd5806cf580cc75d365d543c654552a1f124b4
Evren Yeniev <mail@evren.dev>
2025-05-23 10:40:25 +0200

    feat(reports): implement ReportCard and ReportTable components for enhanced weekly report display

---
+++ src/frontend/src/views/donations/fountains/components/ReportCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ReportTable from "./ReportTable.vue";
+
+defineProps({
+  projects: {
+    type: Array,
+    default: () => []
+  },
+  isoYear: {
+    type: Number,
+    default: new Date().getFullYear(),
+    required: false
+  },
+  isoWeekNumber: {
+    type: Number,
+    default: 1,
+    required: false
+  }
+});
+</script>
+<template>
+  <v-row>
+    <v-col md="12">
+      <v-sheet class="pa-2">
+        <h2 class="text-center mt-2">{{ isoYear }}-KW{{ isoWeekNumber }}</h2>
+        <report-table v-for="item in projects" :key="item.project.name" :item="item" />
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped></style>
--- src/frontend/src/views/donations/fountains/components/index.js
+++ src/frontend/src/views/donations/fountains/components/index.js
@@ -4,3 +4,4 @@ export { default as MediaInformationDialog } from "./MediaInformationDialog.vue"
 export { default as FilterCard } from "./FilterCard.vue";
 export { default as FountainForm } from "./FountainForm.vue";
 export { default as ActionButtons } from "./ActionButtons.vue";
+export { default as ReportCard } from "./ReportCard.vue";
