2abc7170aae9a9658a2c393e4a7d336c0de90a9f
Evren Yeniev <mail@evren.dev>
2025-06-02 14:11:53 +0200

    feat: Implement notification infrastructure with NotificationHub, NotificationSender, SendEventNotificationToClientsHandler, SignalRSettings, and Startup configuration

---
+++ src/backend/Infrastructure/Notifications/NotificationHub.cs
@@ -0,0 +1,35 @@
+﻿using EvrenDev.Application.Common.Exceptions;
+using EvrenDev.Application.Common.Interfaces;
+using Finbuckle.MultiTenant;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace EvrenDev.Infrastructure.Notifications;
+
+[Authorize]
+public class NotificationHub(ITenantInfo? currentTenant, ILogger<NotificationHub> logger) : Hub, ITransientService
+{
+    public override async Task OnConnectedAsync()
+    {
+        if (currentTenant is null)
+        {
+            throw new UnauthorizedException("Authentication Failed.");
+        }
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"GroupTenant-{currentTenant.Id}");
+
+        await base.OnConnectedAsync();
+
+        logger.LogInformation("A client connected to NotificationHub: {connectionId}", Context.ConnectionId);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"GroupTenant-{currentTenant!.Id}");
+
+        await base.OnDisconnectedAsync(exception);
+
+        logger.LogInformation("A client disconnected from NotificationHub: {connectionId}", Context.ConnectionId);
+    }
+}
---
+++ src/backend/Infrastructure/Notifications/NotificationSender.cs
@@ -0,0 +1,47 @@
+﻿using EvrenDev.Application.Common.Interfaces;
+using EvrenDev.Shared.Notifications;
+using Finbuckle.MultiTenant;
+using Microsoft.AspNetCore.SignalR;
+using static EvrenDev.Shared.Notifications.NotificationConstants;
+
+namespace EvrenDev.Infrastructure.Notifications;
+
+public class NotificationSender(IHubContext<NotificationHub> notificationHubContext, ITenantInfo currentTenant)
+    : INotificationSender
+{
+    public Task BroadcastAsync(INotificationMessage notification, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.All
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task BroadcastAsync(INotificationMessage notification, IEnumerable<string> excludedConnectionIds, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.AllExcept(excludedConnectionIds)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToAllAsync(INotificationMessage notification, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.Group($"GroupTenant-{currentTenant.Id}")
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToAllAsync(INotificationMessage notification, IEnumerable<string> excludedConnectionIds, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.GroupExcept($"GroupTenant-{currentTenant.Id}", excludedConnectionIds)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToGroupAsync(INotificationMessage notification, string group, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.Group(group)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToGroupAsync(INotificationMessage notification, string group, IEnumerable<string> excludedConnectionIds, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.GroupExcept(group, excludedConnectionIds)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToGroupsAsync(INotificationMessage notification, IEnumerable<string> groupNames, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.Groups(groupNames)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToUserAsync(INotificationMessage notification, string userId, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.User(userId)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+
+    public Task SendToUsersAsync(INotificationMessage notification, IEnumerable<string> userIds, CancellationToken cancellationToken) =>
+        notificationHubContext.Clients.Users(userIds)
+            .SendAsync(NotificationFromServer, notification.GetType().FullName, notification, cancellationToken);
+}
