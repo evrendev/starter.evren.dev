6ac96f97599cf3d03c2a64b3d37f878069309323
Evren Yeniev <mail@evren.dev>
2025-06-02 14:12:52 +0200

    feat: Implement security headers infrastructure with HeaderNames and SecurityHeaderSettings classes, and middleware for applying headers

---
+++ src/backend/Infrastructure/SecurityHeaders/HeaderNames.cs
@@ -0,0 +1,12 @@
+﻿namespace EvrenDev.Infrastructure.SecurityHeaders;
+
+internal class HeaderNames
+{
+    public const string Xframeoptions = "X-FrameOptions";
+    public const string Xcontenttypeoptions = "X-Content-Type-Options";
+    public const string Referrerpolicy = "Referrer-Policy";
+    public const string Permissionspolicy = "Permissions-Policy";
+    public const string Samesite = "SameSite";
+    public const string Xxssprotection = "X-XSS-Protection";
+    public const string Contentpolicy = "ContentPolicy";
+}
---
+++ src/backend/Infrastructure/SecurityHeaders/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace EvrenDev.Infrastructure.SecurityHeaders;
+
+internal static class Startup
+{
+    internal static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder app, IConfiguration config)
+    {
+        var settings = config.GetSection(nameof(SecurityHeaderSettings)).Get<SecurityHeaderSettings>();
+
+        if (settings?.Enable is true)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (!string.IsNullOrWhiteSpace(settings.XFrameOptions))
+                {
+                    context.Response.Headers[HeaderNames.Xframeoptions] = settings.XFrameOptions;
+                }
+
+                if (!string.IsNullOrWhiteSpace(settings.XContentTypeOptions))
+                {
+                    context.Response.Headers[HeaderNames.Xcontenttypeoptions] = settings.XContentTypeOptions;
+                }
+
+                if (!string.IsNullOrWhiteSpace(settings.ReferrerPolicy))
+                {
+                    context.Response.Headers[HeaderNames.Referrerpolicy] = settings.ReferrerPolicy;
+                }
+
+                if (!string.IsNullOrWhiteSpace(settings.PermissionsPolicy))
+                {
+                    context.Response.Headers[HeaderNames.Permissionspolicy] = settings.PermissionsPolicy;
+                }
+
+                if (!string.IsNullOrWhiteSpace(settings.SameSite))
+                {
+                    context.Response.Headers[HeaderNames.Samesite] = settings.SameSite;
+                }
+
+                if (!string.IsNullOrWhiteSpace(settings.XxssProtection))
+                {
+                    context.Response.Headers[HeaderNames.Xxssprotection] = settings.XxssProtection;
+                }
+
+                await next();
+            });
+        }
+
+        return app;
+    }
+}
