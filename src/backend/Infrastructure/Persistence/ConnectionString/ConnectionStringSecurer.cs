981f4dafe0e562b0db924c77e7afd3a9d5250f44
Evren Yeniev <mail@evren.dev>
2025-06-02 14:16:54 +0200

    feat: Implement ConnectionStringSecurer and ConnectionStringValidator for secure connection string handling

---
+++ src/backend/Infrastructure/Persistence/ConnectionString/ConnectionStringSecurer.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SqlClient;
+using EvrenDev.Application.Common.Persistence;
+using EvrenDev.Infrastructure.Common;
+using Microsoft.Extensions.Options;
+
+namespace EvrenDev.Infrastructure.Persistence.ConnectionString;
+
+public class ConnectionStringSecurer(IOptions<DatabaseSettings> dbSettings) : IConnectionStringSecurer
+{
+    private const string HiddenValueDefault = "*******";
+    private readonly DatabaseSettings _dbSettings = dbSettings.Value;
+
+    public string? MakeSecure(string? connectionString, string? dbProvider)
+    {
+        if (connectionString is null || string.IsNullOrEmpty(connectionString))
+        {
+            return connectionString;
+        }
+
+        if (string.IsNullOrWhiteSpace(dbProvider))
+        {
+            dbProvider = _dbSettings.DbProvider;
+        }
+
+        return dbProvider?.ToLower() switch
+        {
+            DbProviderKeys.SqlServer => MakeSecureSqlServerConnectionString(connectionString),
+            _ => connectionString
+        };
+    }
+
+    private static string MakeSecureSqlServerConnectionString(string connectionString)
+    {
+        var builder = new SqlConnectionStringBuilder(connectionString);
+
+        if (!string.IsNullOrEmpty(builder.Password))
+        {
+            builder.Password = HiddenValueDefault;
+        }
+
+        if (!string.IsNullOrEmpty(builder.UserID))
+        {
+            builder.UserID = HiddenValueDefault;
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ src/backend/Infrastructure/Persistence/ConnectionString/ConnectionStringValidator.cs
@@ -0,0 +1,38 @@
+﻿using EvrenDev.Application.Common.Persistence;
+using EvrenDev.Infrastructure.Common;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace EvrenDev.Infrastructure.Persistence.ConnectionString;
+
+internal class ConnectionStringValidator(IOptions<DatabaseSettings> dbSettings, ILogger<ConnectionStringValidator> logger)
+    : IConnectionStringValidator
+{
+    private readonly DatabaseSettings _dbSettings = dbSettings.Value;
+
+    public bool TryValidate(string connectionString, string? dbProvider = null)
+    {
+        if (string.IsNullOrWhiteSpace(dbProvider))
+        {
+            dbProvider = _dbSettings.DbProvider;
+        }
+
+        try
+        {
+            switch (dbProvider?.ToUpperInvariant())
+            {
+                case DbProviderKeys.SqlServer:
+                    var sqlservercs = new SqlConnectionStringBuilder(connectionString);
+                    break;
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Connection String Validation Exception : {ex.Message}");
+            return false;
+        }
+    }
+}
