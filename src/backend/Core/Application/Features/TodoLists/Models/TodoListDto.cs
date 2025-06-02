a17ef98f25bc3c841421538f5b70e418e3f2a390
Evren Yeniev <mail@evren.dev>
2025-06-02 12:45:42 +0200

    feat: Remove obsolete TodoItem and TodoList command and query classes

--- src/backend/Core/Application/Features/TodoLists/Models/TodoListDto.cs
+++
@@ -1,9 +0,0 @@
-namespace EvrenDev.Application.Features.TodoLists.Models;
-
-public class TodoListDto
-{
-    public Guid Id { get; set; }
-    public string? Title { get; set; }
-    public Colour Colour { get; set; } = Colour.White;
-    public List<BasicTodoItemDto> Items { get; set; } = new();
-}
