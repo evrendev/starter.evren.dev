e35652bfdd04e6292d60b30dd769778d9e75dae3
Evren Yeniev <mail@evren.dev>
2025-06-02 11:17:09 +0200

    feat: Add Migrators project and DesignTimeDbContextFactory for MSSQL

---
+++ src/backend/Migrators/Migrators.MSSQL/DesignTimeDbContextFactory.cs
@@ -0,0 +1,58 @@
+using Backend.Infrastructure.Multitenancy;
+using Backend.Infrastructure.Persistence.Context;
+using Backend.Shared.Multitenancy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EvrenDev.Migrators.Migrators.MSSQL;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TenantDbContext>, IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    TenantDbContext IDesignTimeDbContextFactory<TenantDbContext>.CreateDbContext(string[] args)
+    {
+        var rootPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "../../../"));
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(rootPath)
+            .AddJsonFile("backend/PublicApi/Configurations/database.json")
+            .Build();
+
+        var connectionString = configuration.GetSection("DatabaseSettings:ConnectionString").Value;
+
+        var builder = new DbContextOptionsBuilder<TenantDbContext>();
+        builder.UseSqlServer(connectionString, e =>
+            e.MigrationsAssembly("Migrators.MSSQL"));
+
+        return new TenantDbContext(builder.Options);
+    }
+
+    ApplicationDbContext IDesignTimeDbContextFactory<ApplicationDbContext>.CreateDbContext(string[] args)
+    {
+        var rootPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "../../../"));
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(rootPath)
+            .AddJsonFile("backend/PublicApi/Configurations/database.json")
+            .Build();
+
+        var connectionString = configuration.GetSection("DatabaseSettings:ConnectionString").Value;
+
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        builder.UseSqlServer(connectionString, e =>
+            e.MigrationsAssembly("Migrators.MSSQL"));
+
+        // Varsayılan tenant oluşturuluyor
+        var tenant = new TenantInfo(
+            MultitenancyConstants.Root.Id,
+            MultitenancyConstants.Root.Name,
+            connectionString,
+            MultitenancyConstants.Root.EmailAddress);
+
+        return new ApplicationDbContext(
+            tenant,
+            builder.Options,
+            null!, // ICurrentUser
+            null!, // ISerializerService
+            Microsoft.Extensions.Options.Options.Create(new Backend.Infrastructure.Persistence.DatabaseSettings { DbProvider = "SqlServer" }), // DatabaseSettings
+            null!); // IEventPublisher
+    }
+}
