69b6b875f8c555e61835097a770f9456dd8cc2ec
Evren Yeniev <mail@evren.dev>
2025-06-02 14:10:06 +0200

    feat: Implement middleware infrastructure with ErrorResult, ExceptionMiddleware, RequestLoggingMiddleware, ResponseLoggingMiddleware, and Startup configuration

---
+++ src/backend/Infrastructure/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using EvrenDev.Application.Common.Exceptions;
+using EvrenDev.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using Serilog;
+using Serilog.Context;
+
+namespace EvrenDev.Infrastructure.Middleware;
+
+internal class ExceptionMiddleware(
+    ICurrentUser currentUser,
+    IStringLocalizer<ExceptionMiddleware> localizer,
+    ISerializerService jsonSerializer)
+    : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            var email = currentUser.GetUserEmail() is string userEmail ? userEmail : "Anonymous";
+            var userId = currentUser.GetUserId();
+            var tenant = currentUser.GetTenant() ?? string.Empty;
+            if (userId != Guid.Empty)
+                LogContext.PushProperty("UserId", userId);
+            LogContext.PushProperty("UserEmail", email);
+            if (!string.IsNullOrEmpty(tenant))
+                LogContext.PushProperty("Tenant", tenant);
+            var errorId = Guid.NewGuid().ToString();
+            LogContext.PushProperty("ErrorId", errorId);
+            LogContext.PushProperty("StackTrace", exception.StackTrace);
+            var errorResult = new ErrorResult
+            {
+                Source = exception.TargetSite?.DeclaringType?.FullName,
+                Exception = exception.Message.Trim(),
+                ErrorId = errorId,
+                SupportMessage = localizer["exceptionmiddleware.supportmessage"]
+            };
+            errorResult.Messages!.Add(exception.Message);
+            var response = context.Response;
+            response.ContentType = "application/json";
+            if (exception is not CustomException && exception.InnerException != null)
+            {
+                while (exception.InnerException != null)
+                {
+                    exception = exception.InnerException;
+                }
+            }
+
+            switch (exception)
+            {
+                case CustomException e:
+                    response.StatusCode = errorResult.StatusCode = (int)e.StatusCode;
+                    if (e.ErrorMessages is not null)
+                    {
+                        errorResult.Messages = e.ErrorMessages;
+                    }
+
+                    break;
+
+                case KeyNotFoundException:
+                    response.StatusCode = errorResult.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+
+                default:
+                    response.StatusCode = errorResult.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            Log.Error($"{errorResult.Exception} Request failed with Status Code {context.Response.StatusCode} and Error Id {errorId}.");
+            await response.WriteAsync(jsonSerializer.Serialize(errorResult));
+        }
+    }
+}
---
+++ src/backend/Infrastructure/Middleware/MiddlewareSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace EvrenDev.Infrastructure.Middleware;
+
+public class MiddlewareSettings
+{
+    public bool EnableHttpsLogging { get; set; }
+    public bool EnableLocalization { get; set; }
+}
