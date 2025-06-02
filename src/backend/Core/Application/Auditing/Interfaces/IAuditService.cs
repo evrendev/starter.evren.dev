67010d7f092e6f244a39d5cca0f48a0ac1a944c4
Evren Yeniev <mail@evren.dev>
2025-06-02 12:07:12 +0200

    feat: Add AuditDto and IAuditService for auditing functionality

---
+++ src/backend/Core/Application/Auditing/Interfaces/IAuditService.cs
@@ -0,0 +1,8 @@
+﻿using EvrenDev.Application.Auditing.Entities;
+
+namespace EvrenDev.Application.Auditing.Interfaces;
+
+public interface IAuditService : ITransientService
+{
+    Task<List<AuditDto>> GetUserTrailsAsync(Guid userId);
+}
---
+++ src/backend/Core/Application/Auditing/Queries/Get/GetMyAuditLogsRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using EvrenDev.Application.Auditing.Entities;
+using EvrenDev.Application.Auditing.Interfaces;
+
+namespace EvrenDev.Application.Auditing.Queries.Get;
+
+public class GetMyAuditLogsRequest : IRequest<List<AuditDto>>
+{
+}
+
+public class GetMyAuditLogsRequestHandler(ICurrentUser currentUser, IAuditService auditService)
+    : IRequestHandler<GetMyAuditLogsRequest, List<AuditDto>>
+{
+    public Task<List<AuditDto>> Handle(GetMyAuditLogsRequest request, CancellationToken cancellationToken) =>
+        auditService.GetUserTrailsAsync(currentUser.GetUserId());
+}
