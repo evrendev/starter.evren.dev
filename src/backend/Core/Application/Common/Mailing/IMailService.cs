50b0c749891e5040f48fac913da0a45eee935dc7
Evren Yeniev <mail@evren.dev>
2025-06-02 14:26:59 +0200

    feat: Add email template service, mail service, and mail request model

---
+++ src/backend/Core/Application/Common/Mailing/IMailService.cs
@@ -0,0 +1,6 @@
+﻿namespace EvrenDev.Application.Common.Mailing;
+
+public interface IMailService : ITransientService
+{
+    Task SendAsync(MailRequest request);
+}
---
+++ src/backend/Core/Application/Common/Mailing/MailRequest.cs
@@ -0,0 +1,37 @@
+﻿namespace EvrenDev.Application.Common.Mailing;
+
+public class MailRequest(
+    List<string> to,
+    string subject,
+    string? body = null,
+    string? from = null,
+    string? displayName = null,
+    string? replyTo = null,
+    string? replyToName = null,
+    List<string>? bcc = null,
+    List<string>? cc = null,
+    IDictionary<string, byte[]>? attachmentData = null,
+    IDictionary<string, string>? headers = null)
+{
+    public List<string> To { get; } = to;
+
+    public string Subject { get; } = subject;
+
+    public string? Body { get; } = body;
+
+    public string? From { get; } = from;
+
+    public string? DisplayName { get; } = displayName;
+
+    public string? ReplyTo { get; } = replyTo;
+
+    public string? ReplyToName { get; } = replyToName;
+
+    public List<string> Bcc { get; } = bcc ?? new List<string>();
+
+    public List<string> Cc { get; } = cc ?? new List<string>();
+
+    public IDictionary<string, byte[]> AttachmentData { get; } = attachmentData ?? new Dictionary<string, byte[]>();
+
+    public IDictionary<string, string> Headers { get; } = headers ?? new Dictionary<string, string>();
+}
