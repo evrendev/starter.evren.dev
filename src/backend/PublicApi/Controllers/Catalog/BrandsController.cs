516f7600234769e06f27989be8279dd0d65abe3d
Evren Yeniev <mail@evren.dev>
2025-06-03 10:19:08 +0200

    feat: Implement Brands and Products controllers with CRUD operations and search functionality

---
+++ src/backend/PublicApi/Controllers/Catalog/BrandsController.cs
@@ -0,0 +1,71 @@
+﻿using EvrenDev.Application.Catalog.Brands.Entities;
+using EvrenDev.Application.Catalog.Brands.Queries.Create;
+using EvrenDev.Application.Catalog.Brands.Queries.Delete;
+using EvrenDev.Application.Catalog.Brands.Queries.Generate;
+using EvrenDev.Application.Catalog.Brands.Queries.Get;
+using EvrenDev.Application.Catalog.Brands.Queries.Search;
+using EvrenDev.Application.Catalog.Brands.Queries.Update;
+
+namespace EvrenDev.Host.Controllers.Catalog;
+
+public class BrandsController : VersionedApiController
+{
+    [HttpPost("search")]
+    [MustHavePermission(ApiAction.Search, ApiResource.Brands)]
+    [OpenApiOperation("Search brands using available filters.", "")]
+    public Task<PaginationResponse<BrandDto>> SearchAsync(SearchBrandsRequest request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    [MustHavePermission(ApiAction.View, ApiResource.Brands)]
+    [OpenApiOperation("Get brand details.", "")]
+    public Task<BrandDto> GetAsync(Guid id)
+    {
+        return Mediator.Send(new GetBrandRequest(id));
+    }
+
+    [HttpPost]
+    [MustHavePermission(ApiAction.Create, ApiResource.Brands)]
+    [OpenApiOperation("Create a new brand.", "")]
+    public Task<Guid> CreateAsync(CreateBrandRequest request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpPut("{id:guid}")]
+    [MustHavePermission(ApiAction.Update, ApiResource.Brands)]
+    [OpenApiOperation("Update a brand.", "")]
+    public async Task<ActionResult<Guid>> UpdateAsync(UpdateBrandRequest request, Guid id)
+    {
+        return id != request.Id
+            ? BadRequest()
+            : Ok(await Mediator.Send(request));
+    }
+
+    [HttpDelete("{id:guid}")]
+    [MustHavePermission(ApiAction.Delete, ApiResource.Brands)]
+    [OpenApiOperation("Delete a brand.", "")]
+    public Task<Guid> DeleteAsync(Guid id)
+    {
+        return Mediator.Send(new DeleteBrandRequest(id));
+    }
+
+    [HttpPost("generate-random")]
+    [MustHavePermission(ApiAction.Generate, ApiResource.Brands)]
+    [OpenApiOperation("Generate a number of random brands.", "")]
+    public Task<string> GenerateRandomAsync(GenerateRandomBrandRequest request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpDelete("delete-random")]
+    [MustHavePermission(ApiAction.Clean, ApiResource.Brands)]
+    [OpenApiOperation("Delete the brands generated with the generate-random call.", "")]
+    [ApiConventionMethod(typeof(ApiConventions), nameof(ApiConventions.Search))]
+    public Task<string> DeleteRandomAsync()
+    {
+        return Mediator.Send(new DeleteRandomBrandRequest());
+    }
+}
---
+++ src/backend/PublicApi/Controllers/Catalog/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using EvrenDev.Application.Catalog.Products.Entities;
+using EvrenDev.Application.Catalog.Products.Queries.Create;
+using EvrenDev.Application.Catalog.Products.Queries.Delete;
+using EvrenDev.Application.Catalog.Products.Queries.Export;
+using EvrenDev.Application.Catalog.Products.Queries.Get;
+using EvrenDev.Application.Catalog.Products.Queries.Search;
+using EvrenDev.Application.Catalog.Products.Queries.Update;
+
+namespace EvrenDev.Host.Controllers.Catalog;
+
+public class ProductsController : VersionedApiController
+{
+    [HttpPost("search")]
+    [MustHavePermission(ApiAction.Search, ApiResource.Products)]
+    [OpenApiOperation("Search products using available filters.", "")]
+    public Task<PaginationResponse<ProductDto>> SearchAsync(SearchProductsRequest request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    [MustHavePermission(ApiAction.View, ApiResource.Products)]
+    [OpenApiOperation("Get product details.", "")]
+    public Task<ProductDetailsDto> GetAsync(Guid id)
+    {
+        return Mediator.Send(new GetProductRequest(id));
+    }
+
+    [HttpPost]
+    [MustHavePermission(ApiAction.Create, ApiResource.Products)]
+    [OpenApiOperation("Create a new product.", "")]
+    public Task<Guid> CreateAsync(CreateProductRequest request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpPut("{id:guid}")]
+    [MustHavePermission(ApiAction.Update, ApiResource.Products)]
+    [OpenApiOperation("Update a product.", "")]
+    public async Task<ActionResult<Guid>> UpdateAsync(UpdateProductRequest request, Guid id)
+    {
+        return id != request.Id
+            ? BadRequest()
+            : Ok(await Mediator.Send(request));
+    }
+
+    [HttpDelete("{id:guid}")]
+    [MustHavePermission(ApiAction.Delete, ApiResource.Products)]
+    [OpenApiOperation("Delete a product.", "")]
+    public Task<Guid> DeleteAsync(Guid id)
+    {
+        return Mediator.Send(new DeleteProductRequest(id));
+    }
+
+    [HttpPost("export")]
+    [MustHavePermission(ApiAction.Export, ApiResource.Products)]
+    [OpenApiOperation("Export a products.", "")]
+    public async Task<FileResult> ExportAsync(ExportProductsRequest filter)
+    {
+        var result = await Mediator.Send(filter);
+        return File(result, "application/octet-stream", "ProductExports");
+    }
+}
