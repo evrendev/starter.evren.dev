469e50e7ac597d7ee5abca7041f703ba73f5f752
Evren Yeniev <mail@evren.dev>
2025-07-31 13:01:21 +0200

    feat: remove unused search and analytics components from dashboard layout

--- src/frontend/src/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ src/frontend/src/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -22,31 +22,7 @@ import UserProfile from "@/layouts/components/UserProfile.vue";
           <VIcon icon="bx-menu" />
         </IconBtn>
 
-        <!-- 👉 Search -->
-        <div
-          class="d-flex align-center cursor-pointer ms-lg-n3"
-          style="user-select: none"
-        >
-          <!-- 👉 Search Trigger button -->
-          <IconBtn>
-            <VIcon icon="bx-search" />
-          </IconBtn>
-
-          <span class="d-none d-md-flex align-center text-disabled ms-2">
-            <span class="me-2">Search</span>
-            <span class="meta-key">&#8984;K</span>
-          </span>
-        </div>
-
         <VSpacer />
-
-        <IconBtn
-          href="https://github.com/themeselection/sneat-vuetify-vuejs-admin-template-free"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <VIcon icon="bxl-github" />
-        </IconBtn>
 
         <IconBtn>
           <VIcon icon="bx-bell" />
