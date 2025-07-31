19a9842a7feb22ec5daea6c16dacfe893e4fc43c
Evren Yeniev <mail@evren.dev>
2025-07-31 12:45:21 +0200

    feat: update footer links and content for consistency and branding

--- src/frontend/src/layouts/components/Footer.vue
+++ src/frontend/src/layouts/components/Footer.vue
@@ -1,41 +1,28 @@
 <template>
-  <div class="h-100 d-flex align-center justify-md-space-between justify-center">
+  <div
+    class="h-100 d-flex align-center justify-md-space-between justify-center"
+  >
     <!-- 👉 Footer: left content -->
     <span class="d-flex align-center">
       &copy;
       {{ new Date().getFullYear() }}
       Made With
-      <VIcon
-        icon="bx-heart"
-        color="error"
-        size="1.25rem"
-        class="mx-1"
-      />
-      By <a
-        href="https://themeselection.com"
+      <VIcon icon="bx-heart" color="error" size="1.25rem" class="mx-1" />
+      By
+      <a
+        href="onar.im"
         target="_blank"
         rel="noopener noreferrer"
         class="text-primary ms-1"
-      >ThemeSelection</a>
+        >Onarim GmbH</a
+      >
     </span>
     <!-- 👉 Footer: right content -->
     <span class="d-md-flex gap-x-4 text-primary d-none">
-      <a
-        href="https://themeselection.com/license/"
-        target="noopener noreferrer"
-      >License</a>
-      <a
-        href="https://themeselection.com/"
-        target="noopener noreferrer"
-      >More Themes</a>
-      <a
-        href="https://demos.themeselection.com/sneat-vuetify-vuejs-admin-template/documentation/"
-        target="noopener noreferrer"
-      >Documentation</a>
-      <a
-        href="https://themeselection.com/support/"
-        target="noopener noreferrer"
-      >Support</a>
+      <a href="onar.im/documentation/" target="noopener noreferrer"
+        >Documentation</a
+      >
+      <a href="onar.im/support/" target="noopener noreferrer">Support</a>
     </span>
   </div>
 </template>
