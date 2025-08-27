588ba8d7603a440848c7b1500538be5e108e1360
Evren Yeniev <mail@evren.dev>
2025-08-27 12:20:49 +0200

    feat: update sidebar navigation items and enhance localization support in multiple languages

--- src/frontend/src/layouts/components/NavItems.vue
+++ src/frontend/src/layouts/components/NavItems.vue
@@ -1,34 +1,56 @@
 <script lang="ts" setup>
+const { t } = useI18n();
 import VerticalNavSectionTitle from "@/@layouts/components/VerticalNavSectionTitle.vue";
 import VerticalNavGroup from "@layouts/components/VerticalNavGroup.vue";
 import VerticalNavLink from "@layouts/components/VerticalNavLink.vue";
 </script>
 
 <template>
+  <vertical-nav-link
+    :item="{
+      title: t('admin.components.sidebar.dashboard'),
+      icon: 'bx-home',
+      to: '/admin',
+    }"
+  />
+
+  <vertical-nav-section-title
+    :item="{
+      heading: t('admin.components.sidebar.others'),
+    }"
+  />
+
   <vertical-nav-group
     :item="{
-      title: 'Dashboards',
-      badgeContent: '5',
-      badgeClass: 'bg-error',
-      icon: 'bx-home-smile',
+      title: t('admin.components.sidebar.admin'),
+      icon: 'bx-user',
     }"
   >
     <vertical-nav-link
       :item="{
-        title: 'Analytics',
-        to: '/admin/dashboard',
+        title: t('admin.components.sidebar.tenants'),
+        to: '/admin/admin/tenants',
+      }"
+    />
+
+    <vertical-nav-link
+      :item="{
+        title: t('admin.components.sidebar.roles'),
+        to: '/admin/admin/roles',
+      }"
+    />
+
+    <vertical-nav-link
+      :item="{
+        title: t('admin.components.sidebar.users'),
+        to: '/admin/admin/users',
       }"
     />
   </vertical-nav-group>
 
-  <vertical-nav-section-title
-    :item="{
-      heading: 'Others',
-    }"
-  />
   <vertical-nav-link
     :item="{
-      title: 'Documentation',
+      title: t('admin.components.sidebar.documentation'),
       icon: 'bx-file',
       href: '/documentation',
       target: '_blank',
