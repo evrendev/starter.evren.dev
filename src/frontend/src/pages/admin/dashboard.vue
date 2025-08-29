a9588ec42660514ad70e26bfc78150786eb61c3e
Evren Yeniev <mail@evren.dev>
2025-08-29 10:21:55 +0200

    refactor: add breadcrumbs to admin dashboard and update localization files

--- src/frontend/src/pages/admin/dashboard.vue
+++ src/frontend/src/pages/admin/dashboard.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const { t } = useI18n();
+import CardBasic from "@/views/pages/cards/card-basic/CardBasic.vue";
+import CardNavigation from "@/views/pages/cards/card-basic/CardNavigation.vue";
+import CardSolid from "@/views/pages/cards/card-basic/CardSolid.vue";
+
+const breadcrumbs = ref([
+  { title: t("admin.components.breadcrumbs.home"), to: { path: "/" } },
+  {
+    title: t("admin.components.breadcrumbs.dashboard"),
+    disabled: true,
+  },
+]);
+</script>
 <template>
-  <h1>Admin Dashboard</h1>
+  <v-row>
+    <v-col>
+      <v-breadcrumbs
+        :items="breadcrumbs"
+        class="border rounded"
+        bg-color="primary"
+      >
+        <template v-slot:divider>
+          <v-icon icon="bx-chevron-right"></v-icon>
+        </template>
+      </v-breadcrumbs>
+    </v-col>
+  </v-row>
+
+  <CardBasic />
+  <CardNavigation />
+  <CardSolid />
 </template>
