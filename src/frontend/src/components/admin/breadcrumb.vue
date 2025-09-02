f047cd395ccb74645b706b61e36fb0aefc665758
Evren Yeniev <mail@evren.dev>
2025-09-02 13:18:10 +0200

    refactor: Simplify breadcrumb component template by consolidating prepend slot icons

--- src/frontend/src/components/admin/breadcrumb.vue
+++ src/frontend/src/components/admin/breadcrumb.vue
@@ -12,8 +12,13 @@ defineProps<{
   <v-row>
     <v-col>
       <v-breadcrumbs :items="items" class="border rounded" bg-color="primary">
+        <template v-slot:prepend>
+          <v-icon icon="bx-home" size="small" />
+
+          <v-icon icon="bx-chevron-right" />
+        </template>
         <template v-slot:divider>
-          <v-icon icon="bx-chevron-right"></v-icon>
+          <v-icon icon="bx-chevron-right" />
         </template>
       </v-breadcrumbs>
     </v-col>
