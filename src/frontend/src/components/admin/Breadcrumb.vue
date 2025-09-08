0fab2e0189eafc7e26e12b2a083478af93fa2af7
Evren Yeniev <mail@evren.dev>
2025-09-08 17:43:32 +0200

    feat: Add Breadcrumb component with TypeScript support and Vuetify integration

---
+++ src/frontend/src/components/admin/Breadcrumb.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  items: Array<{
+    title: string;
+    to?: { path: string } | { name: string };
+    disabled?: boolean;
+  }>;
+}>();
+</script>
+
+<template>
+  <v-row>
+    <v-col>
+      <v-breadcrumbs :items="items" class="border rounded" bg-color="primary">
+        <template v-slot:prepend>
+          <v-icon icon="bx-home" size="small" />
+
+          <v-icon icon="bx-chevron-right" />
+        </template>
+        <template v-slot:divider>
+          <v-icon icon="bx-chevron-right" />
+        </template>
+      </v-breadcrumbs>
+    </v-col>
+  </v-row>
+</template>
