c9be10348508c9cdf2a0ce87b0b8b580f0c20499
Evren Yeniev <mail@evren.dev>
2025-09-10 13:07:53 +0200

    feat: add migration V102 to update ValidUpto column type to timestamp with time zone

--- src/backend/Migrators/Migrators.PostgreSQL/Migrations/Tenant/20250910110423_V102.Designer.cs
+++ src/backend/Migrators/Migrators.PostgreSQL/Migrations/Tenant/20250910110423_V102.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EvrenDev.Infrastructure.Multitenancy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Migrators.PostgreSQL.Migrations.Tenant
 {
     [DbContext(typeof(TenantDbContext))]
-    partial class TenantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250910110423_V102")]
+    partial class V102
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/Migrators/Migrators.PostgreSQL/Migrations/Tenant/20250910110423_V102.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.PostgreSQL.Migrations.Tenant
+{
+    /// <inheritdoc />
+    public partial class V102 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValidUpto",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValidUpto",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
