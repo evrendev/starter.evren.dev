4c6d594d81f1020d01968224705385315bbfb6b5
Evren Yeniev <mail@evren.dev>
2025-09-25 09:37:02 +0200

    feat: Refactor CoursesWithCategoriesSpec to implement search functionality and pagination

---
+++ src/backend/Core/Application/Catalog/Courses/Specifications/CoursesBySearchRequestWithCategoriesSpec.cs
@@ -0,0 +1,55 @@
+﻿using EvrenDev.Application.Catalog.Courses.Entities;
+using EvrenDev.Application.Catalog.Courses.Queries.Paginate;
+using EvrenDev.Application.Common.Specification;
+using EvrenDev.Domain.Catalog;
+
+namespace EvrenDev.Application.Catalog.Courses.Specifications;
+
+public class CoursesBySearchRequestWithCategoriesSpec : Specification<Course, CourseDto>
+{
+    public CoursesBySearchRequestWithCategoriesSpec(PaginateCoursesFilter request)
+    {
+        Query.Include(p => p.Category)
+            .Where(course =>
+                (
+                    !request.CategoryId.HasValue
+                    ||
+                    course.CategoryId.Equals(request.CategoryId!.Value)
+                )
+                &&
+                (
+                    string.IsNullOrEmpty(request.Search)
+                    ||
+                    course.Title.ToLower().Contains(request.Search.ToLower())
+                    ||
+                    course.Category.Title.ToLower().Contains(request.Search.ToLower())
+                    ||
+                    (
+                        course.Description != null
+                        &&
+                        course.Description.ToLower().Contains(request.Search.ToLower())
+                    )
+                    ||
+                    (
+                        course.Introduction != null
+                        &&
+                        course.Introduction.ToLower().Contains(request.Search.ToLower())
+                    )
+                    ||
+                    (
+                        course.PreviewVideoUrl != null
+                        &&
+                        course.PreviewVideoUrl.ToLower().Contains(request.Search.ToLower())
+                    )
+                )
+                &&
+                (
+                    !request.Published.HasValue
+                    ||
+                    course.Published == request.Published
+                )
+            )
+            .OrderBy(c => c.Title, !request.HasOrderBy())
+            .PaginateBy(request);
+    }
+}
--- src/backend/Core/Application/Catalog/Courses/Specifications/CoursesWithCategoriesSpec.cs
+++ src/backend/Core/Application/Catalog/Courses/Specifications/CoursesWithCategoriesSpec.cs
@@ -1,55 +1,16 @@
 ﻿using EvrenDev.Application.Catalog.Courses.Entities;
-using EvrenDev.Application.Catalog.Courses.Queries.Paginate;
+using EvrenDev.Application.Catalog.Courses.Queries.Get;
 using EvrenDev.Application.Common.Specification;
 using EvrenDev.Domain.Catalog;
 
 namespace EvrenDev.Application.Catalog.Courses.Specifications;
 
-public class CoursesBySearchRequestWithCategoriesSpec : Specification<Course, CourseDto>
+public class CoursesWithCategoriesSpec : Specification<Course, CourseExportDto>
 {
-    public CoursesBySearchRequestWithCategoriesSpec(PaginateCoursesFilter request)
+    public CoursesWithCategoriesSpec(GetAllCoursesRequest request)
     {
         Query.Include(p => p.Category)
-            .Where(course =>
-                (
-                    !request.CategoryId.HasValue
-                    ||
-                    course.CategoryId.Equals(request.CategoryId!.Value)
-                )
-                &&
-                (
-                    string.IsNullOrEmpty(request.Search)
-                    ||
-                    course.Title.ToLower().Contains(request.Search.ToLower())
-                    ||
-                    course.Category.Title.ToLower().Contains(request.Search.ToLower())
-                    ||
-                    (
-                        course.Description != null
-                        &&
-                        course.Description.ToLower().Contains(request.Search.ToLower())
-                    )
-                    ||
-                    (
-                        course.Introduction != null
-                        &&
-                        course.Introduction.ToLower().Contains(request.Search.ToLower())
-                    )
-                    ||
-                    (
-                        course.PreviewVideoUrl != null
-                        &&
-                        course.PreviewVideoUrl.ToLower().Contains(request.Search.ToLower())
-                    )
-                )
-                &&
-                (
-                    !request.Published.HasValue
-                    ||
-                    course.Published == request.Published
-                )
-            )
-            .OrderBy(c => c.Title, !request.HasOrderBy())
-            .PaginateBy(request);
+            .Where(course => course.Published)
+            .OrderBy(c => c.Title);
     }
 }
