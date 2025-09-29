938a10ccae719eeaccb3b265b8524cfd5527981e
Evren Yeniev <mail@evren.dev>
2025-09-29 12:24:06 +0200

    feat: refactor admin layout to include Loader, AppNotification, and GoToTop components

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-import AppNotification from "@/components/shared/AppNotification.vue";
-import Loader from "@/components/shared/Loader.vue";
-import GoToTop from "@/components/shared/GoToTop.vue";
-</script>
-
 <template>
   <VApp>
-    <Loader />
     <RouterView />
-    <AppNotification />
-    <GoToTop />
   </VApp>
 </template>
--- src/frontend/src/layouts/admin.vue
+++ src/frontend/src/layouts/admin.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
 import Logo from "@/components/shared/Logo.vue";
+import AppNotification from "@/components/shared/AppNotification.vue";
+import Loader from "@/components/shared/Loader.vue";
+import GoToTop from "@/components/shared/GoToTop.vue";
 
 import {
   //Vertical Navbar
@@ -15,49 +18,54 @@ import {
 </script>
 
 <template>
-  <vertical-nav-layout>
-    <template #navbar="{ toggleVerticalOverlayNavActive }">
-      <div class="d-flex h-100 align-center">
+  <div>
+    <Loader />
+    <AppNotification />
+    <vertical-nav-layout>
+      <template #navbar="{ toggleVerticalOverlayNavActive }">
+        <div class="d-flex h-100 align-center">
+          <icon-btn
+            class="ms-n3 d-lg-none"
+            @click="toggleVerticalOverlayNavActive(true)"
+          >
+            <v-icon icon="bx-menu" />
+          </icon-btn>
+
+          <v-spacer />
+
+          <navbar-language-switcher class="me-1" />
+
+          <navbar-theme-switcher class="me-1" />
+
+          <user-profile />
+        </div>
+      </template>
+
+      <template #vertical-nav-header="{ toggleIsOverlayNavActive }">
+        <logo class="app-title-wrapper" />
+
         <icon-btn
-          class="ms-n3 d-lg-none"
-          @click="toggleVerticalOverlayNavActive(true)"
+          class="d-block d-lg-none"
+          @click="toggleIsOverlayNavActive(false)"
         >
-          <v-icon icon="bx-menu" />
+          <v-icon icon="bx-x" />
         </icon-btn>
+      </template>
 
-        <v-spacer />
+      <template #vertical-nav-content>
+        <nav-items />
+      </template>
 
-        <navbar-language-switcher class="me-1" />
+      <router-view />
 
-        <navbar-theme-switcher class="me-1" />
-
-        <user-profile />
-      </div>
-    </template>
-
-    <template #vertical-nav-header="{ toggleIsOverlayNavActive }">
-      <logo class="app-title-wrapper" />
-
-      <icon-btn
-        class="d-block d-lg-none"
-        @click="toggleIsOverlayNavActive(false)"
-      >
-        <v-icon icon="bx-x" />
-      </icon-btn>
-    </template>
-
-    <template #vertical-nav-content>
-      <nav-items />
-    </template>
-
-    <router-view />
-
-    <template #footer>
-      <footer-links />
-    </template>
-  </vertical-nav-layout>
+      <template #footer>
+        <footer-links />
+      </template>
+    </vertical-nav-layout>
+    <GoToTop />
+  </div>
 </template>
 
 <style lang="scss">
-@use "@/assets/styles/admin/default-layout";
+@use "@/assets/styles/admin/default";
 </style>
