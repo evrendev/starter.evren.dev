22df203dbc173cb6e848e024ef0d61ad105efc49
Evren Yeniev <mail@evren.dev>
2026-08-19 19:26:59 +0200

    chore(deps): upgrade to .NET 10 (TFM, EF Core/Npgsql/ASP.NET Core family)

--- src/backend/Migrators/Migrators.PostgreSQL/Migrations/Application/ApplicationDbContextModelSnapshot.cs
+++ src/backend/Migrators/Migrators.PostgreSQL/Migrations/Application/ApplicationDbContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace Migrators.PostgreSQL.Migrations.Application
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("Catalog")
-                .HasAnnotation("ProductVersion", "9.0.5")
+                .HasAnnotation("ProductVersion", "10.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
